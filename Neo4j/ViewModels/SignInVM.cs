9e1741f8e8be38548350c05b1f52a92d5d59eb4a
Bui Minh Thanh Ngoc <thanhngocbuiminh@gmail.com>
2024-09-22 01:20:00 +0700

    Add Signin & Logout Function

---
+++ Neo4j/ViewModels/SignInVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Neo4j.ViewModels
+{
+    public class SignInVM
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid emaild address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$", ErrorMessage = "Password must be at least 6 characters long and contain uppercase letters, lowercase letters, digits, and special characters.")]
+        public string Password { get; set; }
+    }
+}
--- Neo4j/obj/Debug/net8.0/Neo4j.AssemblyInfo.cs
+++ Neo4j/obj/Debug/net8.0/Neo4j.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Neo4j")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de37be22f9f1fca06a36d28ff284ccff42e86920")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f55e84e66f297e7f7d92a5fa87f89e045294ecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Neo4j")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Neo4j")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
