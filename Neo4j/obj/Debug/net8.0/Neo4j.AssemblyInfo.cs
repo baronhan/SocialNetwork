758bdab11edb84e7b3ee06ae2db95c14c19c2b81
Nhan Nguyen <npbaonhan18803@gmail.com>
2024-10-02 09:46:29 +0700

    updated changes

--- Neo4j/obj/Debug/net8.0/Neo4j.AssemblyInfo.cs
+++ Neo4j/obj/Debug/net8.0/Neo4j.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Neo4j")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa80eaa2606d9ce33d58a674fdd40c842ef92e7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f71e94974ec169852882267953881c0616e5150c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Neo4j")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Neo4j")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
